7bf6a06f3514bb6df42036fc1f2bf24f6d632c3a
abdelrahman442 <abdelrahman44200@gmail.com>
2024-03-07 14:21:13 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+// owl carousel
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  });
+});
+// validate form
+var forms = document.querySelectorAll(".needs-validation");
+Array.prototype.slice.call(forms).forEach(function (form) {
+  form.addEventListener(
+    "submit",
+    function (event) {
+      if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+      form.classList.add("was-validated");
+    },
+    false
+  );
+});
